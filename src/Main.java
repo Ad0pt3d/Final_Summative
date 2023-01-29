29bd19a039e16d59e2ae263f7a0b844c8a6b81f6
Ad0pt3d <rafimiah2005@gmail.com>
2023-01-29 15:50:16 -0500

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.time.LocalDateTime;
+
+public class Main extends JFrame {
+
+   public Main() {
+        JButton button1 = new JButton("click");
+        button1.setBounds(100, 100, 100, 30);
+
+        add(button1);
+
+        setTitle("Employee Wage Counter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        pack();
+        setVisible(true);
+   }
+
+   public static void main(String[] args) {
+        new Main();
+   }
+}
+
+class Employee {
+    String name;
+    int id;
+
 }
