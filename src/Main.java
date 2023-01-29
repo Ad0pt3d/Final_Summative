394db0aaa38ecb41d5d7cb22b75880b541c3edb6
Ad0pt3d <rafimiah2005@gmail.com>
2023-01-29 17:19:02 -0500

    Added JSwing UI and Button functionality

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,99 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.time.LocalDateTime;
 
 public class Main extends JFrame {
 
-   public Main() {
-        JButton button1 = new JButton("click");
-        button1.setBounds(100, 100, 100, 30);
+    private JTable employeeTable;
+    private JScrollPane scrollPane;
+    private JButton clockInButton, clockOutButton;
+    private JLabel nameLabel, wageLabel, clockInLabel1, clockInLabel2, clockOutLabel1, clockOutLabel2, hoursLabel, totalLabel;
+    private JTextField nameField, wageField;
 
-        add(button1);
 
-        setTitle("Employee Wage Counter");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
-        pack();
-        setVisible(true);
+    public Main() {
+       String[] columnNames = {"Name", "Wage/Hr", "Clock In", "Clock Out", "Hours Worked", "Total Pay"};
+       String[][] data = {
+               {"Rafi", "15", "00:00", "00:00", "0", "90"},
+               {"Peek", "10", "00:00", "00:00", "0", "60"},
+               {"Taror", "20", "00:00", "00:00", "0", "60"},
+       };
+       employeeTable = new JTable(data, columnNames);
+
+       scrollPane = new JScrollPane(employeeTable);
+       scrollPane.setPreferredSize(new Dimension(300, 150));
+
+       nameLabel = new JLabel("Name:");
+       nameField = new JTextField(10);
+
+       wageLabel = new JLabel("Wage/Hr:");
+       wageField = new JTextField(10);
+
+       clockInLabel1 = new JLabel("Clock In:");
+       clockInLabel2 = new JLabel();
+
+       clockOutLabel1 = new JLabel("Clock Out:");
+       clockOutLabel2 = new JLabel();
+
+       clockInButton = new JButton("Clock In");
+       clockInButton.addActionListener(new ClockInButtonListener());
+
+       clockOutButton = new JButton("Clock Out");
+       clockOutButton.addActionListener(new ClockOutButtonListener());
+
+       JPanel inputPanel = new JPanel();
+       inputPanel.add(nameLabel);
+       inputPanel.add(nameField);
+       inputPanel.add(wageLabel);
+       inputPanel.add(wageField);
+       inputPanel.add(clockInLabel1);
+       inputPanel.add(clockInButton);
+       inputPanel.add(clockOutLabel1);
+       inputPanel.add(clockOutButton);
+
+
+       add(scrollPane, BorderLayout.CENTER);
+       add(inputPanel, BorderLayout.SOUTH);
+
+       setTitle("Employee Wage Counter");
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       pack();
+       setVisible(true);
+    }
+
+   private class ClockInButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+            Date date = new Date();
+            String time = sdf.format(date);
+
+            String name = nameField.getText();
+            String clockIn = time;
+
+            for (int row = 0; row < employeeTable.getRowCount(); row++) {
+                if (name.equals(employeeTable.getValueAt(row, 0))) {
+                    employeeTable.setValueAt(clockIn, row, 1);
+                }
+            }
+        }
+   }
+
+   private class ClockOutButtonListener implements ActionListener {
+       public void actionPerformed(ActionEvent e) {
+           SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+           Date date = new Date();
+           String time = sdf.format(date);
+
+           String name = nameField.getText();
+           String clockOut = time;
+
+           for (int row = 0; row < employeeTable.getRowCount(); row++) {
+               if (name.equals(employeeTable.getValueAt(row, 0))) {
+                   employeeTable.setValueAt(clockOut, row, 2);
+               }
+           }
+       }
    }
 
    public static void main(String[] args) {
@@ -41,6 +115,7 @@ public class Main extends JFrame {
 
 class Employee {
     String name;
-    int id;
+    float wage;
+    float total;
 
 }
