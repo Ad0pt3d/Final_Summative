efb638d0e616fbae38c3c0720cdf4975ae4a298b
Ad0pt3d <rafimiah2005@gmail.com>
2023-02-01 19:16:26 -0500

    Clock in and Clock out removed. Add button for adding employees, update button to update wages of existing employees.

--- src/Main.java
+++ src/Main.java
@@ -11,28 +11,32 @@
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.*;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main extends JFrame {
 
-    private JTable employeeTable;
-    private JScrollPane scrollPane;
-    private JButton clockInButton, clockOutButton;
-    private JLabel nameLabel, wageLabel, clockInLabel1, clockInLabel2, clockOutLabel1, clockOutLabel2, hoursLabel, totalLabel;
-    private JTextField nameField, wageField;
+   DefaultTableModel model = new DefaultTableModel();
+   private JTable employeeTable;
+   private JScrollPane scrollPane;
+   private JButton addButton, updateWageButton;
+   private JLabel nameLabel, wageLabel, hoursLabel, totalLabel;
+   private JTextField nameField, wageField;
 
 
-    public Main() {
-       String[] columnNames = {"Name", "Wage/Hr", "Clock In", "Clock Out", "Hours Worked", "Total Pay"};
-       String[][] data = {
-               {"Rafi", "15", "00:00", "00:00", "0", "90"},
-               {"Peek", "10", "00:00", "00:00", "0", "60"},
-               {"Taror", "20", "00:00", "00:00", "0", "60"},
-       };
-       employeeTable = new JTable(data, columnNames);
+   public Main() {
+       employeeTable = new JTable(model);
 
+       model.addColumn("Employee Name");
+       model.addColumn("Wage/Hr");
+       model.addColumn("Hours Worked");
+       model.addColumn("Total Pay");
+
+       model.addRow(new Object[]{"Rafi", "15", "0", "0"});
+       model.addRow(new Object[]{"Peeker", "15", "0", "0"});
        scrollPane = new JScrollPane(employeeTable);
        scrollPane.setPreferredSize(new Dimension(300, 150));
 
@@ -42,27 +46,19 @@ public class Main extends JFrame {
        wageLabel = new JLabel("Wage/Hr:");
        wageField = new JTextField(10);
 
-       clockInLabel1 = new JLabel("Clock In:");
-       clockInLabel2 = new JLabel();
+       addButton = new JButton("Add");
+       addButton.addActionListener(new AddButtonListener());
 
-       clockOutLabel1 = new JLabel("Clock Out:");
-       clockOutLabel2 = new JLabel();
-
-       clockInButton = new JButton("Clock In");
-       clockInButton.addActionListener(new ClockInButtonListener());
-
-       clockOutButton = new JButton("Clock Out");
-       clockOutButton.addActionListener(new ClockOutButtonListener());
+       updateWageButton = new JButton("Update Wage");
+       updateWageButton.addActionListener(new UpdateWageButtonListener());
 
        JPanel inputPanel = new JPanel();
        inputPanel.add(nameLabel);
        inputPanel.add(nameField);
        inputPanel.add(wageLabel);
        inputPanel.add(wageField);
-       inputPanel.add(clockInLabel1);
-       inputPanel.add(clockInButton);
-       inputPanel.add(clockOutLabel1);
-       inputPanel.add(clockOutButton);
+       inputPanel.add(addButton);
+       inputPanel.add(updateWageButton);
 
 
        add(scrollPane, BorderLayout.CENTER);
@@ -72,55 +68,62 @@ public class Main extends JFrame {
        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        pack();
        setVisible(true);
-    }
+   }
 
-   private class ClockInButtonListener implements ActionListener {
+   private class AddButtonListener implements  ActionListener {
+       public void actionPerformed(ActionEvent e) {
+           String name = nameField.getText();
+           String wageHr = wageField.getText();
+
+           boolean exists = checkIfExist();
+
+           if (!exists) {
+               model.addRow(new Object[]{name, wageHr, "0", "0"});
+           }
+
+       }
+   }
+
+    private class UpdateWageButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-            Date date = new Date();
-            String time = sdf.format(date);
-
             String name = nameField.getText();
-            String clockIn = time;
+            String wageHr = wageField.getText();
 
             for (int row = 0; row < employeeTable.getRowCount(); row++) {
                 if (name.equals(employeeTable.getValueAt(row, 0))) {
-                    employeeTable.setValueAt(clockIn, row, 1);
+                    employeeTable.setValueAt(wageHr, row, 1);
                 }
             }
         }
-   }
+    }
 
-   private class ClockOutButtonListener implements ActionListener {
-       public void actionPerformed(ActionEvent e) {
-           SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
-           Date date = new Date();
-           String time = sdf.format(date);
+   public boolean checkIfExist() {
+       String name = nameField.getText();
 
-           int hour = Integer.parseInt(time.substring(0, 2));
-           int min = Integer.parseInt(time.substring(3, 5));
+       boolean exists = false;
 
-
-
-           String name = nameField.getText();
-           String clockOut = time;
-
-           for (int row = 0; row < employeeTable.getRowCount(); row++) {
-               if (name.equals(employeeTable.getValueAt(row, 0))) {
-                   employeeTable.setValueAt(clockOut, row, 2);
-               }
+       for (int row = 0; row < employeeTable.getRowCount(); row++) {
+           if (name.equals(employeeTable.getValueAt(row, 0))) {
+               System.out.println("Employee Already Exists");
+               exists = true;
            }
        }
+       if (exists) {
+           return true;
+       }
+       else {
+           return false;
+       }
    }
+
 
    public static void main(String[] args) {
         new Main();
    }
+
+   public static void SaveToJSON() {
+
+   }
 }
 
-class Employee {
-    String name;
-    float wage;
-    float total;
 
-}
